2e647210ea7ef36745138001ed541cb7e20b71ba
Lucas Giordano <giordano3102lucas@gmail.com>
2025-07-25 11:16:35 -0700

    add agent file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# merch-demo
+# NotteLabs - Reliable Web Agents
+
+> use our reliable web agent API to order free merch!
+
+**TLDR;** We’re building a full-stack platform to deploy reliable web browser agents that can scrape and take action on any site. This makes it possible to automate hard web workflows even when there’s no API or MCP server. We focus it all on reliability and scale for enterprise.
+
+**Want to try it?** We’re giving away $200 of NotteLabs merch to be ordered by an agent! No catch - this is the only way we thought would incentivize some of you to test our agents and give us some feedback. So here you go - just copy paste and run this code to order (first come, first served)
+
+# How to run the agent
+
+```bash
+pip install notte-sdk
+python agent.py
+```
+
+## Links
+- Read Docs → https://docs.notte.cc 
+- Get an API key → https://console.notte.cc 
+- Let’s talk → [https://cal.com/nottelabs/demo](https://cal.com/team/nottelabs/demo)
+- Or email us: [founders@notte.cc](mailto:founders@notte.cc)
---
+++ agent.py
@@ -0,0 +1,39 @@
+# pip install notte-sdk
+from notte_sdk import NotteClient
+
+# Initialize the notte sdk client
+client = NotteClient(api_key="sk-notte-6a0cf6fbeaec6800d882de6c1e2f2966d7b098dbf7ae56fb21785f24f378581a")
+
+# Personal information for shipping
+name = "<your-name>"
+email = "<your-email>"
+address = "<your-address>"
+phone = "<your-phonenumber>"
+country = "United States"
+
+# Secure vault for the credit card details
+vault = client.Vault("94f42a8f-fd88-4143-991b-9078280230b1")
+# Launch the session & the agent
+with client.Session(browser_type="firefox") as session:
+    agent = client.Agent(
+        session=session,
+        vault=vault,
+        max_steps=20,
+    )
+    # should take a few minutes to complete
+    response = agent.run(
+        url="https://shop.notte.cc",
+        task=f"""
+1. Go to https://shop.notte.cc and wait for the page to fully load
+2. Go to the product page for the cap
+3. Add the item to the cart
+4. Go to the checkout page
+5. Fill the shipping "Email" field with: {email}
+6. Select {country} from the country dropdown (or skip if the country is the US)
+7. Fill the shipping "Full Name" field with: {name}
+8. Fill the shipping "Street address" field with: {address}
+9. Choose the right combobox option for the address. Choosing the right one is important and will fill the remaining fields automatically (i.e state, city, postal code, country). Never use the `fill` action to fill any fields.
+10. Scroll down 2x to the payment section
+11. Fill in the credit card details using the provided values
+12. Click on the "Complete Order" button. 
+""")
